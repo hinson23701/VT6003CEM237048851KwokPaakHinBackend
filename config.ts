c6c8cce96f1a331bc361bdd5307eaa3a4712ea2e
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2024-03-28 01:01:51 +0800

    Delete config.ts

--- config.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  host: 'sql.freedb.tech', //remote db host
-  port: 3306,
-  user: 'freedb_admins', //db username
-  password: 'W%3PBn6JvsXFx!G', //password
-  database: 'freedb_VT6003CEM', //db name
-}
