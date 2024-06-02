25f5d55d47c5d5cdb6acd1f15d309ef110142408
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2024-06-02 07:43:16 +0000

    add the code(upload photo)

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import Koa from "koa";
-import  { RouterContext } from "koa-router";
+import router, { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
 import passport from "koa-passport";
 import cors from '@koa/cors';
-
+import bodyParser from "koa-bodyparser";
 import { router as dogs } from "./routes/dogs";
 import { router as special } from './routes/special';
 import { router as users } from './routes/users';
+import { router as upload } from './routes/upload';
 
 import serve from 'koa-static-folder';
 
@@ -23,14 +24,18 @@ const app: Koa = new Koa();
 router.get('/api/v1', welcomeAPI);*/
 app.use(cors());
 
-//For Document:
+// For Document:
 app.use(serve('./docs'));
-
+app.use(cors());
 app.use(logger());
 app.use(json());
+app.use(bodyParser());
+app.use(router.routes());
 app.use(passport.initialize());
-//app.use(router.routes());
-app.use(cors());
+app.use(dogs.middleware());
+app.use(special.middleware());
+app.use(upload.middleware());
+app.use(users.middleware());
 
 app.use(users.routes());
 app.use(async (ctx: RouterContext, next: any) => {
